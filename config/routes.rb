c3060c0319713792a9be3ab891b9791d38b4aeff
JoshMine <kazeykinvv@yandex.ru>
2013-04-30 02:05:37 +0400

    Home page commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Cport::Application.routes.draw do
-  get "static_pages/home"
+  
+  root :to => 'static_pages#home'
+  
+  match '/home',    to: 'static_pages#home'
 
-  get "static_pages/about"
-
-  get "static_pages/help"
-
-  get "static_pages/contacts"
+  match '/help',    to: 'static_pages#help'
+  
+  match '/about',   to: 'static_pages#about'
+  
+  match '/contacts', to: 'static_pages#contacts'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

2a16cdb15cadc71bbb9c5cded9e87525146a80e7
JoshMine <kazeykinvv@mortsys.ru>
2013-05-22 17:01:01 +0400

    fix

--- db/migrate/20130515185531_add_portfolio_id_to_contact_data.rb
+++ db/migrate/20130515185531_add_portfolio_id_to_contact_data.rb
@@ -1,7 +1,6 @@
 class AddPortfolioIdToContactData < ActiveRecord::Migration
   def change
     add_column :contact_data, :portfolio_id, :integer
-    remove_column :people, :contact_data_id
   end
   
   def down

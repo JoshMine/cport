4b0a605564e6d20ad288beb2ae4d2cdd6d523764
JoshMine <kazeykinvv@mortsys.ru>
2013-05-22 16:58:01 +0400

    fix

--- db/migrate/20130514195701_edit_tables_contact_data_and_portfolio.rb
+++ db/migrate/20130514195701_edit_tables_contact_data_and_portfolio.rb
@@ -1,9 +1,6 @@
 class EditTablesContactDataAndPortfolio < ActiveRecord::Migration
   def change
     add_column :users, :portfolio_id, :integer
-    add_column :people, :first_name, :string
-    add_column :people, :middle_name, :string
-    add_column :people, :last_name, :string
     remove_column :contact_data, :first_name
     remove_column :contact_data, :middle_name
     remove_column :contact_data, :last_name

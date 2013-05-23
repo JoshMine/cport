da7e173cd1b6c8680ff6bbf12d1f7b3f8f496a9e
JoshMine <kazeykinvv@yandex.ru>
2013-05-23 23:52:24 +0400

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_one  :portfolio
-  has_one  :person
+  #has_one  :person
   
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/100x100"
   

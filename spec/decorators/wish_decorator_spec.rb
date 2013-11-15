403c00326e9b1eea1083195ec4ee68860bae692b
Ruslan Voloshin <rebisall@gmail.com>
2013-11-15 22:26:59 +0200

    PF-26 added wish decorator test #resolve

--- spec/decorators/wish_decorator_spec.rb
+++ spec/decorators/wish_decorator_spec.rb
@@ -1,4 +1,35 @@
 require 'spec_helper'
 
 describe WishDecorator do
+  let(:wish) { FactoryGirl.create(:wish) }
+  let(:decorated) { wish.decorate }
+
+  describe "#amount_currency" do
+    it "should should be string" do
+      decorated.amount_currency.should be_kind_of(String)
+    end
+
+    it "should be present" do
+      decorated.amount_currency.should_not be_blank
+    end
+
+    it "should have amount" do
+      decorated.amount_currency.should match(/\d+/)
+    end
+  end
+
+  describe "#need_amount_currency" do
+    it "should should be string" do
+      decorated.need_amount_currency.should be_kind_of(String)
+    end
+
+    it "should be present" do
+      decorated.need_amount_currency.should_not be_blank
+    end
+
+    it "should have amount" do
+      decorated.need_amount_currency.should match(/\d+/)
+    end
+  end
+
 end
